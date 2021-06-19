eff1bcd6020e86afb466b16b0b253caabfe8b534
Dr. ERDI Gergo <gergo@erdi.hu>
2021-06-19 14:41:17 +0800

    Use `signalAutomaton` directly

--- src/simulator-clash.hs
+++ src/simulator-clash.hs
@@ -1,38 +1,39 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
 
-import Clash.Prelude
+import Clash.Prelude hiding (lift)
 
 import Pong
 import RetroClash.Utils
 import RetroClash.VGA
-import RetroClash.Sim.IO
 import RetroClash.Sim.SDL
 import RetroClash.Sim.VGA
 import RetroClash.Sim.VGASDL
 import Control.Monad.State
-import Control.Monad.Loops
-
-board' (up, dn) =
-    let VGAOut{ vgaSync = VGASync{..}, ..} = topEntity clockGen resetGen up dn
-    in bundle (vgaHSync, vgaVSync, bitCoerce <$> bundle (vgaR, vgaG, vgaB))
+import Control.Arrow.Transformer.Automaton
+import Control.Lens
 
 main :: IO ()
 main = do
     buf <- newBufferArray
 
-    sim <- simulateIO (board' . unbundle) (toActive False, toActive False)
+    let board (up, dn) =
+            let VGAOut{ vgaSync = VGASync{..}, ..} = topEntity clockGen resetGen up dn
+            in bundle (vgaHSync, vgaVSync, bitCoerce <$> bundle (vgaR, vgaG, vgaB))
+        sim = signalAutomaton (board . unbundle)
 
-    flip evalStateT initSink $ withMainWindow videoParams $ \events keyDown -> do
+    flip evalStateT (sim, initSink) $ withMainWindow videoParams $ \events keyDown -> do
         guard $ not $ keyDown ScancodeEscape
 
         let up = toActive $ keyDown ScancodeUp
             down = toActive $ keyDown ScancodeDown
+        let sinkVGA = zoom _2 . vgaSinkBuf vga640x480at60 buf
 
-        untilM_ (return ()) $ do
-            sim $ \vgaOut -> do
-                frameDone <- vgaSinkBuf vga640x480at60 buf vgaOut
-                return ((up, down), frameDone)
+        let go (Automaton step) = do
+                let (vga, next) = step (up, down)
+                frameDone <- sinkVGA vga
+                if frameDone then return next else go next
+        _1 <~ (go =<< use _1)
 
         return $ rasterizeBuffer buf
   where
