33a896efd60be1276dd4327f0fc9724793d2d248
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-09-07 19:34:46 +0100

    Improve ArrowDecaPong.tcl to take output_dir, write ./compile-bitstream

--- target/arrow-deca/ArrowDecaPong.tcl
+++ target/arrow-deca/ArrowDecaPong.tcl
@@ -1,6 +1,15 @@
+if {$argc < 1} {
+    puts stderr "Usage: quartus_sh -t $argv0 <output dir>"
+    puts stderr "ERROR: Not enough arguments. Please supply an output directory."
+    exit 1
+}
+
+set output_dir [lindex $argv 0]
+puts stderr "Output directory: $output_dir"
+
 project_open ArrowDecaPong.qpf
 
-set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_dir
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY $output_dir
 
 package require ::quartus::flow
 execute_flow -compile
