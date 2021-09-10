19063335999bed727307116f2ac5e30cd3830e06
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-09-10 18:32:09 +0100

    Add proprietary Terasic I2C controller for HDMI converter

--- target/arrow-deca/Top.v
+++ target/arrow-deca/Top.v
@@ -2,6 +2,7 @@ module Top(
 
     // CLOCK
     input MAX10_CLK1_50,
+    input MAX10_CLK2_50,
 
 	//////////// KEY //////////
 	input 		     [1:0]		KEY,
@@ -48,6 +49,15 @@ module Top(
         , .VGA_RED(HDMI_TX_D[23:16])
         , .VGA_GREEN(HDMI_TX_D[15:8])
         , .VGA_BLUE(HDMI_TX_D[7:0])
+        );
+
+    // HDMI I2C, by Terasic
+    I2C_HDMI_Config u_I2C_HDMI_Config
+        ( .iCLK(MAX10_CLK2_50)
+        , .iRST_N(1'b1)
+        , .I2C_SCLK(HDMI_I2C_SCL)
+        , .I2C_SDAT(HDMI_I2C_SDA)
+        , .HDMI_TX_INT(HDMI_TX_INT)
         );
 
 endmodule
